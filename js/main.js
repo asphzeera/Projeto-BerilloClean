ff85a158da98921f6a39910ac31b3779a12ed576
asphzeera <asaphobrito@gmail.com>
2025-04-25 18:06:41 +0000

    25 de abr. de 2025, 15:06

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.getElementById('mobile-menu-button').addEventListener('click', function() {
+    var menu = document.getElementById('mobile-menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
+
+file
