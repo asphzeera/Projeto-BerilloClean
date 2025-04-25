d4a01498eb442a03ee51b53b2b950f11161092fe
asphzeera <asaphobrito@gmail.com>
2025-04-25 18:51:15 +0000

    25 de abr. de 2025, 15:51

--- js/main.js
+++ js/main.js
@@ -6,34 +6,3 @@ document.getElementById('mobile-menu-button').addEventListener('click', function
         menu.style.display = 'block';
     }
 });
-
-// Como fazer uma janela em Java Script, espero que isso me sirva em algum
-
-const windowElement = document.getElementById('link_lavar');
-
-let isDragging = false;
-let inicialX;
-let inicialY;
-
-windowElement.addEventListener('mousedown', startDragging);
-windowElement.addEventListener('mouseup', stopDragging)
-windowElement.addEventListener('mousemove' , drag);
-
-function startDragging(e){
-    isDragging = true;
-    inicialX = e.clientX - windowElement.getBoundingClientRect().left;
-    inicialY = e.clientY - windowElement.getBoundingClientRect().right;
-}
-
-function stopDragging(e){
-    isDragging = false;
-}
-
-function drag(e){
-    if (isDragging) {
-        const newX = e.clientX - initialX;
-        const newY = e.clientY - initialY;
-        windowElement.style.left = `${newX}px`;
-        windowElement.style.top = `${newY}px`;
-    }
-}
